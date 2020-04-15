e22ed91e9ebc8242f0ca327810308cdfffb12302
osfest <45380878+osfest-bits@users.noreply.github.com>
2020-04-15 09:21:37 +0530

    Create program.java

---
+++ program.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+
+class Main
+{
+  public static void main(String args[])
+  {
+     int num;
+   
+     Scanner in = new Scanner(System.in);
+     num = in.nextInt();
+     System.out.println(num+1);
+  }
+}
